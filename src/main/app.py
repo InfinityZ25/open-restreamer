d9736bd022514521e9eb9b539d01fa77ae9224c9
jcedeno <henixceo@gmail.com>
2023-03-04 05:04:57 -0500

    feat: added mongo connection and object to app

--- src/main/app.py
+++ src/main/app.py
@@ -1,14 +1,26 @@
 from typing import List
+
 import ffmpeg
+
+from pymongo import MongoClient
+from dotenv import dotenv_values
 from flask import Flask, jsonify, request
 
+config = dotenv_values(".env")
 app = Flask(__name__)
+# Store ongoing processes streams in this map. Ideally this should be stored in a database, but in memory copy has to be enough for this example. Make it thread safe if you want to use it in a multi-threaded environment.
 stream_processes = {}
+
+# Connect to the database
+mongo = MongoClient(config['ATLAS_URI'])
+db = mongo[config['DB_NAME']]
 
 
 @app.route('/status/<stream_id>', methods=['GET'])
 def get_stream_status(stream_id):
+    """ Returns the status of the stream with the given stream id."""
     global stream_processes
+    
 
     if stream_id not in stream_processes:
         return jsonify({'message': 'stream not found'}), 404
@@ -31,6 +43,7 @@ def get_streaming_input(rtmp_url_with_key: str) -> ffmpeg.input:
 
 @app.route('/start/<stream_id>', methods=['POST'])
 def start_stream(stream_id):
+    """ Starts a stream with the given stream id and stream keys. """
     global stream_processes
 
     if stream_id in stream_processes and not any(poll is not None and poll.poll() is None for poll in stream_processes[stream_id]):
@@ -48,7 +61,7 @@ def start_stream(stream_id):
     # define the rtmp servers to stream to
     twitch_rtmp_url = f'rtmp://live.twitch.tv/app/{twitch_key}' if twitch_key else None
     youtube_rtmp_url = f'rtmp://a.rtmp.youtube.com/live2/{youtube_key}' if youtube_key else None
-    
+
     # A variable to store the required input streams.
     outputs: List[ffmpeg.input] = []
 
@@ -70,12 +83,12 @@ def start_stream(stream_id):
         )
         outputs.append(youtube_output)
 
-    
     processes = [ffmpeg.run_async(output) for output in outputs]
     stream_processes[stream_id] = processes
 
     return jsonify({'message': f'stream {stream_id} started'})
 
 
+# Programme entry poiny
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # flash stuff
